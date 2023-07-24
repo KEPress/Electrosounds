89dd53b0cab27f9ca9c16c356ceb5c34a1ddceea
KEPress <kepressltd@gmail.com>
2023-07-24 00:20:26 -0400

    profile fix

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment, useContext } from 'react'
 import { Helmet } from 'react-helmet'
-import { useNavigate } from 'react-router-dom'
+//import { useNavigate } from 'react-router-dom'
 import { Form, Row, Col, Button } from 'react-bootstrap'
 import { useFormik, Formik } from 'formik'
 import { toast, ToastContainer } from 'react-toastify'
